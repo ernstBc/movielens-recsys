bf877deb48167b73a479bef7582445020ac8b0b0
Ernesto B <ernestobravc@gmail.com>
2026-08-25 17:26:49 -0600

    GetData Implemented

---
+++ src/data_pipeline/processing_data.py
@@ -0,0 +1,23 @@
+import random
+import pandas as pd
+
+
+
+class ProcessData:
+    def __init__(self, raw_data_path:str, destination_path:str, mode:str):
+        self.raw_data_path = raw_data_path
+        self.destination_path = destination_path
+        self.mode = mode
+
+
+
+    def process(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    pass
+
+
